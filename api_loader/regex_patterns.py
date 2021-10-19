78e42ad21f41feb9dfbbed0605161a7476a04b9b
filipploch <filip.ploch@gmail.com>
2021-10-19 21:32:11 +0200

    refactor regex_patterns.py

--- api_loader/regex_patterns.py
+++ api_loader/regex_patterns.py
@@ -1,5 +1,6 @@
-import enum
+from enum import Enum
 
 
-class RegexPatterns(enum.Enum):
-    
+class RegexPatterns(Enum):
+    primary_pair_regex = r'^(\d[\d\ \.\-x\^]*)(([\w\.]*)[\w\+\ \(\)\.]*\/(([\d\.]*)? ?([\w]*))|([\w\.\%]*))?'
+    brackets_regex = r'^\((.*)\) ?\/((\d*\.*\,*\d*) *(.*))'
